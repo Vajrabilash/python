fc4b79cec03361ec19a177a30c55d423ab12f9e9
vajrabilash <degapoguvajrabilash@gmail.com>
2024-05-28 09:51:25 +0530

    modified addition

---
+++ addition.py
@@ -0,0 +1,7 @@
+#python program to print addition of two number
+value1 = int(input("Enter first number: "))            #reading the input from user for both values
+value2 = int(input("Enter second number: "))
+#logic declaration and storing in answer varaible
+answer = value1 + value2  # adding two values
+print("The addition of {} and {} is {}".format(value1,value2,answer));  #Displaying the answer
+
